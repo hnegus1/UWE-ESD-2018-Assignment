02e5b9653bc14822709f37b8dd2c07aaf3b80384
hnegus1 <harrynegus@hotmail.co.uk>
2018-11-13 22:02:23 +0000

    Made a start on using the Google Map Distance Matrix API. Added a paid column in the journey table. (You'll have to update the database) Added a dependencies folder for libraries that the project needs.

--- UWE-ESD-2018-Assignment/UWE-ESD-2018-Assignment-war/src/java/com/GoogleMapsTest.java
+++ UWE-ESD-2018-Assignment/UWE-ESD-2018-Assignment-war/src/java/com/GoogleMapsTest.java
@@ -5,23 +5,28 @@
  */
 package com;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.ws.WebServiceRef;
-import ws.AlphaCabWS_Service;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  *
  * @author Harry
  */
-public class WebServiceExample extends HttpServlet {
-
-    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/AlphaCabWS/AlphaCabWS.wsdl")
-    private AlphaCabWS_Service service;
+public class GoogleMapsTest extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -32,19 +37,45 @@ public class WebServiceExample extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+    private static String definitelyNotAnAPIKey = "askmeonfbforit"; //oc do not steal 
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        
+        URL url = new URL("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=" + definitelyNotAnAPIKey);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.connect();
+        
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+           content.append(inputLine);
+        }
+        in.close();
+        con.disconnect();
+        
+        JsonObject jsonObject = new JsonParser().parse(content.toString()).getAsJsonObject();
+        
+        String dest = jsonObject.get("destination_addresses").getAsString();
+        String origin = jsonObject.get("origin_addresses").getAsString();
+        String time = jsonObject.getAsJsonArray("rows").get(0).getAsJsonObject().get("elements").getAsJsonArray().get(0).getAsJsonObject().get("duration").getAsJsonObject().get("text").getAsString();
+        
+        
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet WebServiceExample</title>");            
+            out.println("<title>Servlet GoogleMapsTest</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet WebServiceExample at " + request.getContextPath() + "</h1>");
-            out.println(addList("SELECT * FROM USERS", "STATIC"));          
+            out.println("<h1>Servlet GoogleMapsTest at " + request.getContextPath() + "</h1>");
+            out.println("<p>To get from " + origin + " to " + dest + ", it will take " + time + ".</p>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -88,12 +119,5 @@ public class WebServiceExample extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
-    private String addList(java.lang.String sql, java.lang.String type) {
-        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
-        // If the calling of port operations may lead to race condition some synchronization is required.
-        ws.AlphaCabWS port = service.getAlphaCabWSPort();
-        return port.addList(sql, type);
-    }
 
 }
