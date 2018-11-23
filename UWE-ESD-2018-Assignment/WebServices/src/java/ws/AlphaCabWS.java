ae262ea2327656514d3ad904ab6095eadab7dfbc
hnegus1 <harrynegus@hotmail.co.uk>
2018-11-23 19:13:30 +0000

    Fixed a small issue in the addList web service where multiple radio options could be selected

--- UWE-ESD-2018-Assignment/WebServices/src/java/ws/AlphaCabWS.java
+++ UWE-ESD-2018-Assignment/WebServices/src/java/ws/AlphaCabWS.java
@@ -113,7 +113,7 @@ public class AlphaCabWS {
                                 ID=(int)results.getObject(col);
                             }
                         }
-                        sb.append("<th><input type='radio' value='"+ID+"'></th>");
+                        sb.append("<th><input type='radio' name='ID' value='"+ID+"'></th>");
                         sb.append("</tr>");
                         row++;
                     }
